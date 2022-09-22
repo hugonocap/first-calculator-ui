7237f695011022867c6d033144a97110eb596a45
Hugo Montana <benzlbenny2k@gmail.com>
2022-09-22 16:00:36 +0300

    calculator-is-working

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutt/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -98,8 +99,26 @@ class _HomePageState extends State<HomePage> {
                   } else if (index == 1) {
                     //* Delete button
                     return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          userQuestion = userQuestion.substring(
+                              0, userQuestion.length - 1);
+                        });
+                      },
                       buttonText: buttons[index],
                       color: Colors.red,
+                      textColor: Colors.grey.shade300,
+                    );
+                  } else if (index == buttons.length - 1) {
+                    //* Equal button
+                    return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          equalPressed();
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: Colors.deepPurple,
                       textColor: Colors.grey.shade300,
                     );
                   } else {
@@ -133,5 +152,17 @@ class _HomePageState extends State<HomePage> {
       return true;
     }
     return false;
+  }
+
+  void equalPressed() {
+    String finalQuestion = userQuestion;
+    finalQuestion = finalQuestion.replaceAll('x', '*');
+
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    userAnswer = eval.toString();
   }
 }
